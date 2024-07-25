ec00f05ebda30213453467ca7c6cc284aaf02f29
유종화 <sola201@naver.com>
2024-07-26 00:57:36 +0900

    ✨feature/중고거래 페이지 C / R 완료

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 // src/components/Header.js
 import React from 'react';
 import { Container, Navbar, Form, FormControl, Button, Nav } from 'react-bootstrap';
-import { useNavigate,Link  } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import '../css/Header.css';
 
 const Header = () => {
@@ -16,6 +16,7 @@ const Header = () => {
                 </Navbar.Brand>
                 <Nav className="me-auto">
                     <Nav.Link onClick={() => navigate('/item')}>중고거래</Nav.Link>
+                    <Nav.Link onClick={() => navigate('/my-posts')}>내가 쓴 글</Nav.Link>
                 </Nav>
                 <Form className="d-flex ms-auto">
                     <FormControl
@@ -29,7 +30,7 @@ const Header = () => {
                 </Form>
                 <div className="ms-3">
                     <Button id="login-button" variant="primary" className="me-2" as={Link} to="/login">로그인</Button>
-                    <Button id="signup-button" variant="secondary" className="me-2" as={Link} to ="/signup">회원가입</Button>
+                    <Button id="signup-button" variant="secondary" className="me-2" as={Link} to="/signup">회원가입</Button>
                     <Button id="chat-button" variant="secondary">채팅하기</Button>
                 </div>
             </Container>
